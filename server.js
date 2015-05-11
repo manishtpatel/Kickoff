417b4d3d87e5a5e48966f1ad9d80e1cf96db5188
Manish Patel <manishtpatel@yahoo.com>
2015-05-11 10:32:59 -0400

    checkin

--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 var express = require('express');
 var app = express();
 
+// global controller
+app.get('/*',function(req,res,next){
+    res.header('Access-Control-Allow-Origin' , 'http://localhost:3000' );
+    next(); // http://expressjs.com/guide.html#passing-route control
+});
+
 app.use(express.static('./'));
 
 app.listen(3000);
